5c593aa8b081e68600754a18c660a90518f8ac9c
Simon de Boer <sdeboer@athenasoftware.net>
2015-04-14 09:03:25 -0400

    Getting separate team streaks now.
    But points are wrong and they still don't have positional calculations.

--- server/src/objectives.erl
+++ server/src/objectives.erl
@@ -7,7 +7,7 @@
 % client API
 -export([
 				 times/1,
-				 times_match/1
+				 times_match/1, streaks/1
 				]).
 
 % gen_server API
@@ -17,11 +17,35 @@
 				 code_change/3
 				]).
 
+-record(streak, {
+					start,
+					finish,
+					score = 0,
+					events = [],
+					players = [],
+					positions = []
+				 }).
+
+-record(time, {
+					stamp,
+					position,
+					type,
+					score,
+					team = undefined,
+					attackers = [],
+					victims = []
+				 }).
+
+-define(STREAK_GAP, 20000).
+
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 times_match(Match) ->
 	gen_server:call(?MODULE, {process, Match}).
+
+streaks(Match) ->
+	gen_server:call(?MODULE, {streaks, Match}).
 
 times(Mid) ->
 	gen_server:call(?MODULE, {times, Mid}).
@@ -35,12 +59,159 @@ handle_call({times, Mid}, _F, S) ->
 	end;
 
 handle_call({process, Match}, _F, S) ->
-	{reply, process(Match), S}.
+	{reply, process(Match), S};
 
-process(M) ->
+handle_call({streaks, Match}, _F, S) ->
+	{reply, get_streaks(Match), S}.
+
+get_streaks(M) ->
 	Timeline = proplists:get_value(<<"timeline">>, M),
 	Frames = proplists:get_value(<<"frames">>, Timeline, []),
-	lists:foldl(fun(F, A) -> process_frame(F, A) end, [], Frames).
+	Times = lists:foldl(fun process_frame/2, [], Frames),
+	Participants = proplists:get_value(<<"participants">>, M),
+	{Atimes, Btimes} = lists:partition(fun(T) ->
+																				 case T#time.team of
+																					 undefined ->
+																						 Pid = lists:nth(1, T#time.attackers),
+																						 partition_events(Pid, Participants);
+
+																					 Tid ->
+																						 Tid =:= 100
+																				 end
+																		 end, Times),
+
+	Astreaks = lists:foldl(fun find_streaks/2, [], Atimes),
+	Bstreaks = lists:foldl(fun find_streaks/2, [], Btimes),
+	[12, Astreaks, Bstreaks].
+
+partition_events(Pid, [P | Rest]) ->
+	case proplists:get_value(<<"participantId">>, P) of
+		Pid ->
+			proplists:get_value(<<"teamId">>, P, undefined) =:= 100;
+		_Pid ->
+			partition_events(Pid, Rest)
+	end.
+
+process(M) ->
+	[Frames, Streaks] = get_streaks(M),
+	Players = proplists:get_value(<<"participantIdentities">>, M),
+	Participants = proplists:get_value(<<"participants">>, M),
+
+	Scores = [ process_player(P) || P <- Players ],
+	S2 = [ add_teams(Scores, P) || P <- Participants ],
+
+	add_scores(Frames, Streaks, S2),
+	Streaks.
+
+find_streaks(T, [])->
+	[#streak{
+			start = T#time.stamp,
+			finish = T#time.stamp,
+			positions = [T#time.position],
+			events = [T#time.type],
+			score = T#time.score,
+			players = T#time.attackers
+		 }];
+
+find_streaks(T, [S | Rest])->
+	New = T#time.stamp,
+	Diff = abs(New - S#streak.finish),
+	lager:debug("STREAK ~p, ~p, ~p", [S#streak.finish, New, New - S#streak.finish]),
+	if
+		Diff < ?STREAK_GAP ->
+
+
+			S2 = S#streak{
+						 finish = New,
+						 positions = [T#time.position | S#streak.positions],
+						 players = [T#time.attackers | S#streak.players],
+						 events = [T#time.type | S#streak.events],
+						 score = T#time.score + S#streak.score
+						},
+			[S2 | Rest];
+
+		true ->
+			Sprev = S#streak{
+								players = lists:usort( lists:flatten(S#streak.players) )
+							 },
+			Snew = S#streak{
+							 start = T#time.stamp,
+							 finish = T#time.stamp,
+							 events = [T#time.type],
+							 positions = [T#time.position],
+							 players = T#time.attackers
+							},
+			[Snew, Sprev | Rest]
+	end.
+
+add_scores([], _, Scores) -> Scores;
+add_scores(_, [], Scores) -> Scores;
+
+add_scores([A, B | Frest], [T | Trest], Scores) ->
+
+	if
+		T#time.stamp > B#time.stamp -> add_scores([B, Frest], [T | Trest], Scores);
+
+		true ->
+			Origin = T#time.position,
+			Attackers = T#time.attackers,
+			Apid = lists:nth(1, Attackers),
+
+			P = proplists:get_value(<<"particpantFrames">>, A, []),
+			_A2 = lists:foldl(fun(E, Acc) ->
+														Pid = proplists:get_value(<<"participantID">>, E),
+														case lists:member(Acc, Pid) of
+															true -> Acc;
+															false ->
+																case is_same_team(Pid, Apid, Scores) of
+																	false -> Acc;
+																	true ->
+																		Pos = proplists:get_value(<<"position">>, E),
+																		case is_local(Pos, Origin) of
+																			true -> [Pid | Acc];
+																			false -> Acc
+																		end
+																end
+														end
+												end, Attackers, P)
+	end,
+
+	add_scores([A, B | Frest], [T | Trest], Scores).
+
+is_local(A, B) -> A =:= B.
+
+process_player(Pdata) ->
+	Player = proplists:get_value(<<"player">>, Pdata),
+	[
+	 { pid, proplists:get_value(<<"participantId">>, Pdata)},
+	 { sid, proplists:get_value(<<"summonerId">>, Player)},
+	 { name, proplists:get_value(<<"summonerName">>, Player)},
+	 { score, 0 },
+	 { position, undefined }
+	].
+
+is_same_team(A, B, Scores) ->
+	participant_team(A, Scores) =:= participant_team(B, Scores).
+
+participant_team(Pid, [H | T]) ->
+	case proplists:get_value(pid, H) of
+		Pid ->
+			proplists:get_value(team, H);
+		_ ->
+			participant_team(Pid, T)
+	end.
+
+add_teams(Scores, P) ->
+	Team = proplists:get_value(<<"teamId">>, P),
+	Pid = proplists:get_value(<<"participantId">>, P),
+
+	lists:filtermap(fun (E) ->
+											case proplists:get_value(pid, E) of
+												Pid -> {true, [{team, Team} | E]};
+
+												_ -> true
+											end
+									end, Scores).
 
 process_frame(Frame, Acc) ->
 	case proplists:get_value(<<"events">>, Frame, undefined) of
@@ -56,26 +227,27 @@ process_event(E, Acc) ->
 	Score = case proplists:get_value(<<"eventType">>, E, undefined) of
 						<<"WARD_PLACED">> -> 
 							Creator = proplists:get_value(<<"creatorId">>, E),
-							[{type, ward_placed}, {score, 1}, {players, [Creator]}];
+							#time{type = ward_placed, score = 1, attackers = [Creator]};
 
 						<<"WARD_KILL">> -> 
-							[{type, ward_kill}, {score, 2}, {players, [Killer]}];
+							#time{type = ward_kill, score = 2, attackers = [Killer]};
 
 						<<"CHAMPION_KILL">> -> 
 							Victim = proplists:get_value(<<"victimId">>, E),
-							[{type, champion}, {score, 3}, {players, [Killer, Victim | Assists] }];
+							#time{type = champion, score = 3, attackers = [Killer | Assists], victims = [Victim]};
 
 						<<"ELITE_MONSTER_KILL">> -> 
 							case proplists:get_value(<<"monsterType">>, E, undefined) of
 								<<"BARON_NASHOR">> ->
-									[{type, baron}, {score, 5}, {players, [Killer | Assists]}];
+									#time{type = baron, score = 5, attackers = [Killer | Assists]};
 								<<"DRAGON">> ->
-									[{type, dragon}, {score, 3}, {players, [Killer | Assists]}];  % unless its 5th then this should be 5
+									#time{type = dragon, score = 3, attackers = [Killer | Assists]};  % unless its the 5th dragon..maybe
 								_ -> undefined
 							end;
 
 						<<"BUILDING_KILL">> -> 
-							[{type, building}, {score, 10}, {players, [Killer | Assists]}];
+							Team = proplists:get_value(<<"teamId">>, E),
+							#time{type = building, score = 10, attackers = [Killer | Assists], team = Team};
 
 						_ -> undefined
 					end,
@@ -84,9 +256,9 @@ process_event(E, Acc) ->
 		undefined -> Acc;
 
 		S ->
-			S2 = [ {time, proplists:get_value(<<"timestamp">>, E)},
-						 {position, proplists:get_value(<<"position">>, E)} |
-						 S ],
+			lager:debug("attacker ~p / ~p", [S#time.type, S#time.attackers]),
+			S2 = S#time{stamp = proplists:get_value(<<"timestamp">>, E),
+									position = proplists:get_value(<<"position">>, E)},
 			[S2 | Acc]
 	end.
 
@@ -106,7 +278,7 @@ process_event(E, Acc) ->
 
 %process_participant(undefined, Acc) -> Acc;
 %process_participant({Id, Details}, Acc) ->
-	%[{id, Id}, {position, proplists:get_value(<<"position">>, Details, undefined)} | Acc].
+%[{id, Id}, {position, proplists:get_value(<<"position">>, Details, undefined)} | Acc].
 
 handle_cast(_R, _S) -> {stop, bad_cast}.
 
